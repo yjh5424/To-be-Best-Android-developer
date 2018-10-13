a611a4c7a0809438232abdd32d3ce9bac1dfca4f
YunJungHyeon <yjh54240@naver.com>
2018-10-13 15:33:21 +0900

    add 안드로이드 컴포넌트

---
+++ Kotlin/app/src/main/java/com/example/yunjunghyeon/kotlinexample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.yunjunghyeon.kotlinexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+
+    val value1 : String by lazy { "a" }
+
+    lateinit var value2 : String
+
+    init {
+        value2 = "b"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        Log.d("Log",value1)
+        Log.d("Log",value2)
+    }
+}
--- Testing/DemoTestApp/app/src/main/java/com/example/yunjunghyeon/demotestapp/domain/a.kt
+++
@@ -1,2 +0,0 @@
-package com.example.yunjunghyeon.demotestapp.domain
-
